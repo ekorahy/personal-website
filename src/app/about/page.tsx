8838c5cd3b2807fc6d0a9c533f7fa11dc7765730
ekorahy <ekorahy@gmail.com>
2024-06-20 04:34:05 +0700

    feat: add header section for article pages

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import HeaderPage from "@/components/molecule/HeaderPage";
+
+export default function Page() {
+  return (
+    <article>
+      <HeaderPage title="About" description="Short story about me." />
+    </article>
+  );
+}
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,12 @@
+import HeaderPage from "@/components/molecule/HeaderPage";
+
+export default function Page() {
+  return (
+    <article>
+      <HeaderPage
+        title="Blog"
+        description="Share my thoughts and tutorials about software engineering."
+      />
+    </article>
+  );
+}
