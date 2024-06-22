3b06054027cc927f1c170b17497fdeb7326e2eee
ekorahy <ekorahy@gmail.com>
2024-06-23 06:53:44 +0700

    styling: change styling for light dan dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} text-slate-500`}>
+      <body className={`${inter.className}`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
