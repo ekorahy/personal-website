33faa3c6cb58cb6bfa16f014993bb3bc510669b4
ekorahy <ekorahy@gmail.com>
2024-07-04 05:42:41 +0700

    styling: change alignment for favorite tech stack to center when on a small device

--- src/components/molecule/FavoriteTechList.tsx
+++ src/components/molecule/FavoriteTechList.tsx
@@ -3,7 +3,7 @@ import FavoriteTechItem from "../atom/FavoriteTechItem";
 
 export default function FavoriteTechList() {
   return (
-    <div className="mt-2 flex gap-3">
+    <div className="mt-2 flex justify-center gap-3 sm:justify-start">
       {favoriteTechs.map(({ id, name, logo, color }) => (
         <FavoriteTechItem key={id} name={name} logo={logo} color={color} />
       ))}
