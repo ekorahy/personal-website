4fba01ac93ef5fa2fa8546f1fb781ebc7c2144da
ekorahy <ekorahy@gmail.com>
2024-07-31 13:36:08 +0700

    style: add hover styling for social media button

--- src/components/atoms/SocialMediaItem.tsx
+++ src/components/atoms/SocialMediaItem.tsx
@@ -16,7 +16,7 @@ export default function SocialMediaItem({
   return (
     <Link
       href={link}
-      className="flex w-full items-center justify-center gap-2 rounded-md px-3 py-2 text-white sm:w-max"
+      className="flex w-full items-center justify-center gap-2 rounded-md px-3 py-2 text-white sm:w-max transition-transform duration-300 hover:scale-105"
       style={linkStyle}
     >
       {logo} {title}
